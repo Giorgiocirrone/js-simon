36c4037af0e0780a1c211b927a3d7a003cfc359c
Giorgiocirrone <cirronegiorgio@gmail.com>
2024-09-01 11:26:36 +0200

    validazione input numeri

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@
 
 //Recupero elementi dalla traccia 
 const countdownElement = document.getElementById('countdown');
-const numberListElement = document.getElementById('numbers-list');
-const form = document.getElementById('answers-form');
+const numberListElement = document.getElementById('number-list');
+const form = document.getElementById('anwer-form');
 const messageElement = document.getElementById('message');
 const inputGroup = document.getElementById('input-group');
 const instructionElement = document.getElementById('instructions');
-
+const inputFields = document.querySelectorAll('input');
 
 //impostazioni iniziali quello che io so da traccia 
 
@@ -23,8 +23,71 @@ countdownElement.innerHTML = time;
 
 //genero 5 numeri random 
 
-const numbers = getDifferentRandomNumbers(min, max, totaleNumbers); ù
+const numbers = getDifferentRandomNumbers(min, max, totaleNumbers);
 console.log(numbers);//piccola validazione 
 
 
+//inizio il conto alla rovescia 
 
+//inserisco i numeri in pagina 
+
+let items = '';
+for (let i = 0; i < numbers.length; i++) {
+
+    items += `<li>${numbers[i]}</li>`;
+}
+
+numberListElement.innerHTML = items;
+
+const interval = setInterval(() => {
+
+    countdownElement.innerText = --time; //decrementa e poi lo stampa 
+
+    if (time === 0) {
+        clearInterval(interval);
+
+        form.classList.remove('d-none');
+        numberListElement.classList.add('d-none');
+    }
+
+
+}, 1000)
+
+
+
+
+
+// controllo del form
+
+
+form.addEventListener('submit', event => {
+
+    event.preventDefault();//blocco l'invio del form 
+    const userGuesses = [];
+
+
+    for (let i = 0; i < inputFields.length; i++) {
+        const input = inputFields[i];//scorro 
+
+
+        const value = parent(input.value);
+        if (!isNaN(value && value >= min && value <= max && !userGuesses.includes(value)))//controllo dei duplicati 
+            //prendo il valore 
+            userGuesses.push(value);//devo mettere qui quello converito 
+
+    }
+
+
+    if (userGuesses.length !== inputFields.length) {
+
+        //così facendo posso inserire un messaggio di errore 
+        messageElement.innerText = 'sono stati inseriti numeri non validi o dublicati ';
+
+        return;// non ho capito 
+    }
+
+    //validazione 
+    console.log(userGuesses);
+
+
+}) 
